8a64a3ead7d6df6fa96b8360af271a767fbe7096
“prince <princegahlawat004@gmail.com>
2025-02-15 23:03:20 -0500

    Updates

---
+++ precache-manifest.fd60f9977904b1613d4426607a0038c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "754212744961a1cf1e254aa636623c62",
+    "url": "/COMP2156/index.html"
+  },
+  {
+    "revision": "27141c6a4c8730507c31",
+    "url": "/COMP2156/static/css/main.30d17bd7.chunk.css"
+  },
+  {
+    "revision": "1582e325d1bcf091e744",
+    "url": "/COMP2156/static/js/2.ca150098.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/COMP2156/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "27141c6a4c8730507c31",
+    "url": "/COMP2156/static/js/main.62d6e38c.chunk.js"
+  },
+  {
+    "revision": "de1a19f294be11126051",
+    "url": "/COMP2156/static/js/runtime~main.6619830e.js"
+  }
+]);
